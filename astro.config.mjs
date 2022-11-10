421bb282435f343374fc797ef8b03ea0a5fce4fb
Gigi <47110839+DarthGigi@users.noreply.github.com>
2022-11-10 11:54:59 +0100

    Fix paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,5 @@ export default defineConfig({
     // Enable React for the Algolia search component.
     react(),
   ],
-  site: `https://rayfield-docs.vercel.app/en/introduction`,
+  site: `https://rayfield-docs.vercel.app/`,
 });
