a8190a2f0e5185c58dde4df59e2d11c3d7f4eeec
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-03-29 23:39:22 +0200

    Visual updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,20 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   site: `https://rayfield.dev/`,
   integrations: [
-    // Enable Preact to support Preact JSX components.
-    preact(),
-    // Enable React for the Algolia search component.
-    react(),
-    sitemap(),
-    robotsTxt(),
-  ],
+  // Enable Preact to support Preact JSX components.
+  preact(),
+  // Enable React for the Algolia search component.
+  react(), sitemap(), robotsTxt(), tailwind()]
 });
--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -42,14 +42,13 @@ export default function Search() {
 
   return (
     <>
-      <button type="button" ref={searchButtonRef} onClick={onOpen} className="search-input">
-        <svg width="24" height="24" fill="none">
-          <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+      <button type="button" ref={searchButtonRef} onClick={onOpen} className="search-input ">
+        <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M3 11.3516C3 16.5078 7.19531 20.7031 12.3516 20.7031C14.3906 20.7031 16.2539 20.0469 17.7891 18.9453L23.5547 24.7227C23.8242 24.9922 24.1758 25.1211 24.5508 25.1211C25.3477 25.1211 25.8984 24.5234 25.8984 23.7383C25.8984 23.3633 25.7578 23.0234 25.5117 22.7773L19.7812 17.0117C20.9883 15.4414 21.7031 13.4844 21.7031 11.3516C21.7031 6.19531 17.5078 2 12.3516 2C7.19531 2 3 6.19531 3 11.3516ZM5.00391 11.3516C5.00391 7.29688 8.29688 4.00391 12.3516 4.00391C16.4062 4.00391 19.6992 7.29688 19.6992 11.3516C19.6992 15.4062 16.4062 18.6992 12.3516 18.6992C8.29688 18.6992 5.00391 15.4062 5.00391 11.3516Z" fill="currentColor" />
         </svg>
-
         <span>Search</span>
 
-        <span className="search-hint">
+        <span className="search-hint !hidden">
           <span className="sr-only">Press </span>
 
           <kbd>/</kbd>
@@ -57,7 +56,6 @@ export default function Search() {
           <span className="sr-only"> to search</span>
         </span>
       </button>
-
       {isOpen &&
         createPortal(
           <DocSearchModal
